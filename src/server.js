3a5c9c6feb0fee01f21ca5d3649bcaf2183d1dec
Setyo Agung <ubuntu@ip-172-31-26-213.ap-southeast-1.compute.internal>
2021-04-11 08:43:59 +0000

    update ip

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const routes = require('./routes');
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '52.77.239.21',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '172.31.26.213',
     routes: {
       cors: {
         origin: ['*'],
